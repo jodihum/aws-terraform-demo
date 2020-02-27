e91fb3fa21fe848f59e4bcbb4f9ef78300e4b848
Jodi Humphreys <jodi.humphreys@gmail.com>
2020-02-27 10:20:04 +0000

    added lambda permission

--- modules/services/lambda/main.tf
+++ modules/services/lambda/main.tf
@@ -31,6 +31,17 @@ resource "aws_iam_role" "lambda_role" {
 
 }
 
+resource "aws_lambda_permission" "apigw" {
+    statement_id  = "AllowAPIGatewayInvoke"
+    action        = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.hello_world.arn
+    principal     = "apigateway.amazonaws.com"
+
+    # The /*/* portion grants access from any method on any resource
+    # within the API Gateway "REST API".
+#    source_arn = "${aws_api_gateway_deployment.resource_name_of_deployment.execution_arn}/*/*"
+}
+
 
 output "invoke_arn" {
   value  = aws_lambda_function.hello_world.invoke_arn
