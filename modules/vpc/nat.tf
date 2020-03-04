855cd8d85e9dc8283e6270d46757b737945fc0cb
Jodi Humphreys <jodi.humphreys@gmail.com>
2020-03-04 17:07:24 +0000

    cleaning up

--- modules/vpc/nat.tf
+++ modules/vpc/nat.tf
@@ -64,6 +64,6 @@ resource "aws_security_group" "access_via_nat" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [local.all_ips]
   }
 }
